09e3d7ebec28ab03f6fc741e331699fb8ea496b9
vijayalakshmiajithan3 <158257076+vijayalakshmiajithan3@users.noreply.github.com>
2025-04-11 16:03:02 +0530

    Update HouseApplet.java

--- HouseApplet.java
+++ HouseApplet.java
@@ -12,27 +12,27 @@ public class HouseApplet extends Applet implements MouseListener
     { 
 
         g.setColor(Color.YELLOW); 
-        g.fillRect(100, 150, 200, 150); // House body 
+        g.fillRect(100, 150, 200, 150);
  
     
         g.setColor(Color.RED); 
-        int[] xPoints = {100, 200, 300}; // Triangle X coordinates 
-        int[] yPoints = {150, 50, 150};  // Triangle Y coordinates 
+        int[] xPoints = {100, 200, 300};
+        int[] yPoints = {150, 50, 150}; 
         g.fillPolygon(xPoints, yPoints, 3); 
  
       
         g.setColor(Color.WHITE); 
-        g.fillRect(120, 170, 40, 40); // Left window 
-        g.fillRect(240, 170, 40, 40); // Right window 
+        g.fillRect(120, 170, 40, 40); 
+        g.fillRect(240, 170, 40, 40); 
  
       
         g.setColor(doorColor); 
-        g.fillRect(180, 220, 40, 80); // Door 
+        g.fillRect(180, 220, 40, 80); 
       
         g.setColor(Color.BLACK); 
         g.fillOval(215, 260, 5, 5); 
     } 
-    public void mouseClicked(MouseEvent e)  // Change door color on click 
+    public void mouseClicked(MouseEvent e)  
      { 
         doorColor = (doorColor == Color.BLUE) ? Color.RED : Color.BLUE; 
         repaint();     } 

57d34d8029cfa21972a5287aa18ecfa3a8719abb
vijayalakshmiajithan3 <158257076+vijayalakshmiajithan3@users.noreply.github.com>
2025-05-02 15:48:05 +0530

    Add files via upload

---
+++ DirectoryList.java
@@ -0,0 +1,34 @@
+import java.io.File;
+public class DirectoryList
+{
+public static void main(String[] args)
+{
+String dirPath = "/home/mca/Desktop/viji/java rec";
+listDirectoryContents(dirPath);
+}
+public static void listDirectoryContents(String dirPath)
+{
+File directory = new File(dirPath);
+if (directory.exists() && directory.isDirectory( ) )
+{
+System.out.println("Listing subdirectories and files in: " + dirPath);
+File [ ] directoryContents = directory.listFiles();
+if (directoryContents != null)
+{
+for (File item: directoryContents)
+{
+if (item .isDirectory())
+{
+System.out.println("Directory: " + item.getName());
+}
+else
+{
+System.out.println("File: " + item.getName());
+}
+}
+}
+}
+else
+{
+System.out.println("Directory does not exist or is not a directory");
+}}}
